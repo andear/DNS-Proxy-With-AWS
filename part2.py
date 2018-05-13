58e7ce32531e6b4bf211eae842e0e045aa8bd6d4
Han Wang <wanghan.wh06@gmail.com>
2018-05-13 16:13:19 -0500

    finished test of Part2

--- part2.py
+++ part2.py
@@ -1,13 +1,12 @@
 import socket
 import sys
 
-def UDPDNSProxy(port):
-    host = ''
+def UDPDNSProxy():
     udpsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     print 'Socket created'
 
     try:
-        udpsocket.bind((host, port))
+        udpsocket.bind(('', 53))
         print 'Bind succeed.'
     except socket.error, msg:
         print 'Bind failed.'
@@ -18,7 +17,7 @@ def UDPDNSProxy(port):
         print data.encode("hex"), address
 
         response = getResponse(data)
-        print "answer:", response.encode("hex")
+        print "get response"
 
         if response:
             udpsocket.sendto(response, address)
@@ -35,15 +34,15 @@ def getResponse(query):
     upstreamServer = ('8.8.8.8', 53)
     querysocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     querysocket.sendto(query, upstreamServer)
-    data = querysocket.recvfrom(1024)
+    try :
+        data, add = querysocket.recvfrom(1024)
+    except Exception, timeout:
+        print "Request time out"
     return data
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.exit(1)
 
-    port = int(sys.argv[1])
-    UDPDNSProxy(port)
+    UDPDNSProxy()
 
     sys.exit(0)
