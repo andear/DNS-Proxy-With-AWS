442fb6bfd13b32e0028f8a92a5c43b66ac4ffcd9
Han Wang <wanghan.wh06@gmail.com>
2018-05-14 16:14:21 -0500

    updated part3

--- part3.py
+++ part3.py
@@ -10,7 +10,7 @@ def DNSProxy():
     try:
         tcpsocket.bind((host, 53))
         print 'TCP socket bind succeed.'
-    except socket.error, msg:
+    except Exception, msg:
         print 'TCP socket bind failed.'
         sys.exit()
 
@@ -23,7 +23,7 @@ def DNSProxy():
     try:
         udpsocket.bind((host, 53))
         print 'UDP socket bind succeed.'
-    except socket.error, msg:
+    except Exception, msg:
         print 'UDP socket bind failed.'
         sys.exit()
 
@@ -35,58 +35,45 @@ def DNSProxy():
         for sock in readable:
             if sock == tcpsocket:
                 connection, address = sock.accept()
-                request = connection.recv(1024)
-                print "TCP recved."
-                tcpresponse = getTCPResponse(request)
-                print "TCP Responsed"
+                request = connection.recv(4096)
+                print(request)
+                print "TCP request recved."
 
-                connection.sendall(tcpresponse)
+                upstreamServer = ('8.8.8.8', 53)
+                querysocket = socket(AF_INET, SOCK_STREAM)
+                querysocket.connect(upstreamServer)
+                querysocket.sendall(request)
+                empty = False
+                while True:
+                    tcpresponse = querysocket.recv(1024)
+                    print "TCP request sent"
+                    if tcpresponse:
+                        connection.sendall(tcpresponse)
+                        print "TCP Responsed"
+                    else:
+                        if not empty:
+                            empty = True
+                            continue
+                        else :
+                            break
+
+                querysocket.close()
+
                 connection.close()
-                # inputs.append(connection)
 
             elif sock == udpsocket:
                 data, address = sock.recvfrom(1024)
                 # print data.encode("hex"), address
+                upstreamServer = ('8.8.8.8', 53)
+                # querysocket = socket(AF_INET, SOCK_DGRAM)
+                sock.sendto(data, upstreamServer)
 
-                udpresponse = getUDPResponse(data)
+                udpresponse, add = sock.recvfrom(1024)
                 print "UDP Responsed"
 
                 sock.sendto(udpresponse, address)
             else:
                 print "incorrect socket:", sock
-
-
-def getTCPResponse(query):
-    '''
-    Get TCP response from upstream DNS server
-    :param query: TCP query going to be sent
-    :return:
-    '''
-    upstreamServer = ('8.8.8.8', 53)
-    querysocket = socket(AF_INET, SOCK_STREAM)
-    querysocket.connect(upstreamServer)
-    querysocket.sendall(query)
-    try:
-        data = querysocket.recv(5000)
-    except Exception, timeout:
-        print "Request time out"
-    return data
-
-
-def getUDPResponse(query):
-    '''
-    Get UDP response from upstream DNS server
-    :param query: UDP query going to be sent
-    :return:
-    '''
-    upstreamServer = ('8.8.8.8', 53)
-    querysocket = socket(AF_INET, SOCK_DGRAM)
-    querysocket.sendto(query, upstreamServer)
-    try:
-        data, add = querysocket.recvfrom(1024)
-    except Exception, timeout:
-        print "Request time out"
-    return data
 
 
 if __name__ == '__main__':
